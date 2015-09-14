affe7f676b87f628bef7ea4cdd2679918129b81f
abdelrhamane <abdelrhamane@hotmail.fr>
2015-09-14 11:46:07 +0200

    add: Librairie Core ORMLite

--- app/src/main/java/com/abdelrhamane/dufaux/jass/models/record.java
+++ app/src/main/java/com/abdelrhamane/dufaux/jass/models/record.java
@@ -3,11 +3,20 @@ package com.abdelrhamane.dufaux.jass.models;
 /**
  * Created by abdelrhamanebenhammou on 14/09/15.
  */
-public class record {
+import com.j256.*;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "record")
+class record {
+    @DatabaseField
     private String name;
+    @DatabaseField(index = true)
     private String filename;
+    @DatabaseField
     private boolean favorite;
     //durate in seconde
+    @DatabaseField
     private int durate;
 
 
